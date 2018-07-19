13ab749d98ba54875416e208df9a9552c072e798
Ariba728 <41363734+Ariba728@users.noreply.github.com>
2018-07-19 14:51:28 +0530

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,13 +1,14 @@
 package Child;
-
 public class Person {
-	String firstname;
+        String firstname;
 	String lastName;
 	String address;
 	char gender;
 	String iris;
-	String firis;
-        String miris;
+	
+        private Person father;
+        private Person mother;
+        
         public String getName(){
         return firstname;
         }
@@ -19,14 +20,7 @@ public class Person {
         {
         return address;
         }
-        public String getMiris()
-        {
-        return miris;
-        }
-         public String getFiris()
-        {
-        return firis;
-        }
+        
          public char getGender()
          {
          return gender;
@@ -40,38 +34,68 @@ public class Person {
         public void setaddress(String address) {
                   this.address= address;
                 }
-        public void setMiris(String miris) {
-                  this.miris= miris;
-                }
-        public void setFiris(String firis) {
-                  this.firis= firis;
-                }
+        
          public void setGender(char gender) {
                   this.gender= gender;
                 }
-         public void Color_Of_Iris(String mother_iris,char gender, String father_iris)
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getIris() {
+        return iris;
+    }
+
+    public void setIris(String iris) {
+        this.iris = iris;
+    }
+
+    public Person getFather() {
+        return father;
+    }
+
+    public void setFather(Person father) {
+        this.father = father;
+    }
+
+    public Person getMother() {
+        return mother;
+    }
+
+    public void setMother(Person mother) {
+        this.mother = mother;
+    }
+         
+         
+         
+         public void Color_Of_Iris()
          
          {
-              if(mother_iris.equals(father_iris))
+              if(mother.getIris().equals(father.getIris()))
                 {
-                   if(gender == 'M'){
-                iris = "Brown";
+                   if(this.gender == 'M'){
+                this.iris = "Brown";
                 }
-                   else if(gender=='F')
+                   else if(this.gender=='F')
                    {
-                       iris= "Not defined";
+                       this.iris= "Not defined";
                  
                    }
                 
                 }
-                else if (!mother_iris.equals(father_iris))
+                else if (!mother.getIris().equals(father.getIris()))
                 {
-                    if(gender == 'M')
+                    if(this.gender == 'M')
                     {
-                        iris= mother_iris;
+                        iris= mother.getIris();
                     }
                     else
-                        iris = father_iris;
+                        iris = father.getIris();
                 }
          
                 System.out.println("Iris Color :" +iris);
