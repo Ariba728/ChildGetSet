f5cf5ef2e23d97d0a455913c49403558f2554041
Ariba728 <41363734+Ariba728@users.noreply.github.com>
2018-07-19 14:52:27 +0530

    Update PersonModel.java

--- PersonModel.java
+++ PersonModel.java
@@ -1,30 +1,34 @@
-
 package Child;
 
 public class PersonModel {
 
 	public static void main(String[] args) {
-		
-            
+
             Person child = new Person();
+            
             String FirstName = child.getName();
-            String LastName = child.getLastName();
-            String Address = child.getAddress();
-            String Mother_Iris = child.getMiris();
-            String Father_Iris = child.getFiris();
             char gender = child.getGender();
-            
             child.setName("Ariba");
-            child.setlastName("Khan");
-            child.setaddress("Goa"); 
-            child.setMiris("Black");
-            child.setFiris("Black");
-            child.setGender('M');
-            System.out.println("The details of child are :");
-            System.out.println("Name:" +child.firstname+" "+child.lastName);
-            System.out.println("Address :" +child.address);
+            child.setGender('F');
+           
+            Person father = new Person();
+            String LastName = father.getLastName();
+            String Address = father.getAddress();
+            father.setlastName("Khan");
+            father.setaddress("Mumbai"); 
+            father.setIris("Black");
+
+            Person mother=new Person();
+            mother.setIris("Blue");
             
-            child.Color_Of_Iris(child.miris, child.gender,child.firis);
+            
+            System.out.println("The details of child are :");
+            System.out.println("Name:" +child.firstname+" "+father.lastName);
+            System.out.println("Address :" +father.address);
+            
+            child.setFather(father);
+            child.setMother(mother);
+            child.Color_Of_Iris();
             
 
 	}
